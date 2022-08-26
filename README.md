3cf9dc53109c4435ea547da28c69559d4ee9d847
George Termentzoglou <gtermentzoglou@novibet.com>
2022-08-26 11:06:05 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RandomTextLabel
+
+A description of this package.
---
+++ Sources/RandomTextLabel/RandomTextLabel.swift
@@ -0,0 +1,6 @@
+public struct RandomTextLabel {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
