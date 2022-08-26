667bffc3df5c4103bae6d83590c72eb102f9fb80
George Termentzoglou <gtermentzoglou@novibet.com>
2022-08-26 11:25:16 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # RandomTextLabel
 
-A description of this package.
+This library helps you easily stress test your layout constraints, by assigning random long strings to UILabels.
+The type of content is respected, to help readability of the UI. (A number is replaced with a big number)
+
+For detailed explanation see https://medium.com/@george.termentzoglou/ios-layouttest-with-method-swizzling-d56da55ef68a
+
+Copyright 2022 George Termentzoglou
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
--- Sources/RandomTextLabel/RandomTextLabel.swift
+++ Sources/RandomTextLabel/RandomTextLabel.swift
@@ -1,6 +1,46 @@
-public struct RandomTextLabel {
-    public private(set) var text = "Hello, World!"
+import UIKit
 
-    public init() {
+private var randomValueKey : UInt8 = 3
+
+extension UILabel {
+    
+    @objc dynamic func _swizzled_setText(_ text: String?) {
+        guard text != nil else { return }
+        let hasRandomValue = objc_getAssociatedObject(self, &randomValueKey) as? Bool
+        guard self.text == nil || hasRandomValue == nil || hasRandomValue == false else { return }
+        objc_setAssociatedObject(self, &randomValueKey, true, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+        var randomText = ""
+        if text?.isNumeric ?? false {
+            randomText = "\(Float.random(in: 0..<10000))"
+        } else {
+            let wordLength = Int.random(in: 1..<50)
+            randomText = String.random(length: wordLength)
+        }
+        _swizzled_setText(randomText)
+    }
+        
+    static func randomText() {
+        let selector1 = #selector(setter: UILabel.text)
+        let selector2 = #selector(UILabel._swizzled_setText(_:))
+        let originalMethod = class_getInstanceMethod(UILabel.self, selector1)!
+        let swizzleMethod = class_getInstanceMethod(UILabel.self, selector2)!
+        method_exchangeImplementations(originalMethod, swizzleMethod)
+    }
+}
+
+extension String {
+    static func random(length: Int = 20) -> String {
+       let base = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+       var randomString: String = ""
+       for _ in 0..<length {
+           let randomValue = arc4random_uniform(UInt32(base.count))
+           randomString += "\(base[base.index(base.startIndex, offsetBy: Int(randomValue))])"
+       }
+       return randomString
+    }
+}
+extension String {
+    var isNumeric : Bool {
+        return !self.isEmpty && self.rangeOfCharacter(from: NSCharacterSet.letters) == nil
     }
 }
